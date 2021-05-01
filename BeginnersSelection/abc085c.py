c2fc7deaa9f2595159d1ca4f430ae0a5888f6a40
yochimonji <yochimonji@gmail.com>
2021-05-01 11:54:12 +0900

    add BeginnersSelection_abc085c

---
+++ BeginnersSelection/abc085c.py
@@ -0,0 +1,9 @@
+n, y = map(int, input().split())
+
+for i in range(n+1):
+    for j in range(n+1-i):
+        k = n-i-j
+        if y == 10000*i + 5000*j + 1000*k:
+            print(i, j, k)
+            exit()
+print(-1, -1, -1)
