54059d8072b10c7b2c179c75c81113a71d0cca8a
yochimonji <yochimonji@gmail.com>
2021-05-16 18:38:35 +0900

    update abc201_a

--- abc201/a.py
+++ abc201/a.py
@@ -1,6 +1,7 @@
 a = list(map(int, input().split()))
 
-if (a[2]-a[1] == a[1]-a[0]) or (a[2]-a[0] == a[0]-a[1]) or (a[1]-a[2] == a[2]-a[0]) or (a[1]-a[0] == a[0]-a[2]) or (a[0]-a[1] == a[1]-a[2]) or (a[0]-a[2] == a[2]-a[1]):
-    print('Yes')
-else:
-    print('No')
+a.sort()
+ans = 'No'
+if a[2]-a[1] == a[1]-a[0]:
+    ans = 'Yes'
+print(ans)
