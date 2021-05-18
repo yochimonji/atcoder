68a9662fd558b02f7be1337916ffb99390e9ff94
yochimonji <yochimonji@gmail.com>
2021-05-18 22:31:22 +0900

    add abc196_abc

---
+++ abc196/b.py
@@ -0,0 +1,5 @@
+x = input()
+dot_pos = x.find('.')
+if dot_pos != -1:
+    x = x[:dot_pos]
+print(x)
---
+++ abc196/c.py
@@ -0,0 +1,5 @@
+n = int(input())
+for i in range(1, 1000001):
+    if int(str(i) * 2) > n:
+        print(i-1)
+        exit()
